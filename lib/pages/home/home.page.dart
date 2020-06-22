24c3d90ea07869ff643535e5f505a537cff3c61c
João Pedro <jpfs@icomp.ufam.edu.br>
2020-06-21 20:34:51 -0400

    refatoracao

--- lib/pages/home/home.page.dart
+++ lib/pages/home/home.page.dart
@@ -48,51 +48,9 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                  width: size.width,
-                  height: 180,
-                  child: ListView.builder(
-                    itemCount: action.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                          padding: EdgeInsets.only(right: 10.0),
-                          child: Stack(children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.asset(
-                                'assets/images/${action[index]}',
-                                width: 170,
-                                height: 180,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Positioned(
-                                top: 10,
-                                right: 10,
-                                child: Container(
-                                  width: 25,
-                                  height: 25,
-                                  decoration: BoxDecoration(
-                                      color: style.bg,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: Icon(Icons.favorite,
-                                      color: (index % 2) == 0
-                                          ? style.accent
-                                          : style.primary,
-                                      size: 15),
-                                )),
-                            Positioned(
-                                bottom: 10,
-                                left: 10,
-                                child: Text('Filme filme filme',
-                                    style: TextStyle(
-                                        color: style.primary,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w900)))
-                          ]));
-                    },
-                  ),
+                ListMovies(
+                  movies: action,
+                  size: size,
                 ),
                 SizedBox(
                   height: 40,
@@ -110,51 +68,9 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                  width: size.width,
-                  height: 180,
-                  child: ListView.builder(
-                    itemCount: comedy.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                          padding: EdgeInsets.only(right: 10.0),
-                          child: Stack(children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.asset(
-                                'assets/images/${comedy[index]}',
-                                width: 170,
-                                height: 180,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Positioned(
-                                top: 10,
-                                right: 10,
-                                child: Container(
-                                  width: 25,
-                                  height: 25,
-                                  decoration: BoxDecoration(
-                                      color: style.bg,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: Icon(Icons.favorite,
-                                      color: (index % 2) == 0
-                                          ? style.accent
-                                          : style.primary,
-                                      size: 15),
-                                )),
-                            Positioned(
-                                bottom: 10,
-                                left: 10,
-                                child: Text('Filme filme filme',
-                                    style: TextStyle(
-                                        color: style.primary,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w900)))
-                          ]));
-                    },
-                  ),
+                ListMovies(
+                  movies: comedy,
+                  size: size,
                 ),
                 SizedBox(
                   height: 40,
@@ -172,51 +88,9 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                  width: size.width,
-                  height: 180,
-                  child: ListView.builder(
-                    itemCount: animated.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                          padding: EdgeInsets.only(right: 10.0),
-                          child: Stack(children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.asset(
-                                'assets/images/${animated[index]}',
-                                width: 170,
-                                height: 180,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Positioned(
-                                top: 10,
-                                right: 10,
-                                child: Container(
-                                  width: 25,
-                                  height: 25,
-                                  decoration: BoxDecoration(
-                                      color: style.bg,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: Icon(Icons.favorite,
-                                      color: (index % 2) == 0
-                                          ? style.accent
-                                          : style.primary,
-                                      size: 15),
-                                )),
-                            Positioned(
-                                bottom: 10,
-                                left: 10,
-                                child: Text('Filme filme filme',
-                                    style: TextStyle(
-                                        color: style.primary,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w900)))
-                          ]));
-                    },
-                  ),
+                ListMovies(
+                  movies: animated,
+                  size: size,
                 )
               ],
             ),
@@ -248,6 +122,62 @@ class CategorySeparator extends StatelessWidget {
               fontWeight: FontWeight.w500),
         ),
       ],
+    );
+  }
+}
+
+class ListMovies extends StatelessWidget {
+  final Size size;
+  final List movies;
+
+  ListMovies({@required this.size, @required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width,
+      height: 180,
+      child: ListView.builder(
+        itemCount: movies.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: Stack(children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/${movies[index]}',
+                    width: 170,
+                    height: 180,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                    top: 10,
+                    right: 10,
+                    child: Container(
+                      width: 25,
+                      height: 25,
+                      decoration: BoxDecoration(
+                          color: style.bg,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Icon(Icons.favorite,
+                          color:
+                              (index % 2) == 0 ? style.accent : style.primary,
+                          size: 15),
+                    )),
+                Positioned(
+                    bottom: 10,
+                    left: 10,
+                    child: Text('Filme filme filme',
+                        style: TextStyle(
+                            color: style.primary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w900)))
+              ]));
+        },
+      ),
     );
   }
 }
